bee2f393f1c149efbd5d763f7ff97e022d6d63d6
Juhwooce <juhwooce@gmail.com>
2023-10-12 17:32:39 -0400

    Update search.py

--- search.py
+++ search.py
@@ -163,23 +163,18 @@ def generateSubTopics(qry, model="gpt-3.5-turbo-16k", max_tokens=500):
     subTopics = generate_content(prompt, model=model, max_tokens=max_tokens)
     return subTopics
 
-def toTable(dict):
-    r = []
-    for obj in len(dict):
-        r.append(dict[obj])
-    return r
 
 def main():
     qry = st.text_input(
-        "What do you want the main topic of the articles to be? v18\n",
+        "What do you want the main topic of the articles to be? v19\n",
         key="query",
     )
 
     if qry:
         st.title(f"Article about {qry}")  # add a title
         categories = generateCategories(qry)
+        st.write(categories)
         categories = literal_eval(categories[0])
-        toTable(categories)
         st.write(categories)
         
         with st.form("Category Select"):
