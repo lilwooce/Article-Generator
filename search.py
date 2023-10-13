97f3efa80901fbffc4e9357b115ccaa9cb67497f
Juhwooce <juhwooce@gmail.com>
2023-10-12 21:49:57 -0400

    Update search.py

--- search.py
+++ search.py
@@ -165,7 +165,7 @@ def generateSubTopics(qry, model="gpt-3.5-turbo-16k", max_tokens=500):
 
 def main():
     qry = st.text_input(
-        "What do you want the main topic of the articles to be? v55\n",
+        "What do you want the main topic of the articles to be? v60\n",
         key="query",
     )
 
@@ -201,10 +201,10 @@ def main():
         #mainCat = WPUploader.createWPCategory(qry)
         #st.write(f"Main Category ID is {mainCat}")
         for cat in st.session_state.chosenCategories:
+            subTopics = generateSubTopics(cat)
+            subTopics = literal_eval(subTopics[0])
+            st.write(subTopics)
             with st.form(f"Sub Topic Select for: {cat}"):
-                subTopics = generateSubTopics(cat)
-                subTopics = literal_eval(subTopics[0])
-                st.write(subTopics)
                 chosenTopics  = st.multiselect("Which of these Sub Topics would you like", options=subTopics, key=cat)
 
                 submitted = st.form_submit_button(label="Submit Topics")
