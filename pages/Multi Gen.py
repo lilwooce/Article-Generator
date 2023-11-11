1ed1b65e5a473ba2cefe464bfe0f68ee8be2aeeb
Juhwooce <kryptstep@gmail.com>
2023-11-11 00:18:31 -0500

    Update Multi Gen.py

--- pages/Multi Gen.py
+++ pages/Multi Gen.py
@@ -32,13 +32,13 @@ def generate_content(prompt, model="gpt-3.5-turbo", max_tokens=2000, temperature
     print(response)
     return response.strip().split('\n')
 
-'''def quickArticleCreate(qry, model="gpt-3.5-turbo-16k", max_tokens=3000):
-    prompt = f"Act as a skilled content writer who is proficient in SEO writing and has excellent English language skills. To get started, please create two tables. The first table should contain an outline of the article, and the second table should contain the article itself. Please use Markdown language to bold the heading of the second table. The article should be 4,000 words long, unique, SEO-optimized, and human-written in English. It should cover the given topic and include at least 15 headings and subheadings (including H1, H2, H3, and H4 headings). Please compose the article in your own words, avoiding copying and pasting from other sources. When producing content, each paragraph should be at least 250 words long, please consider complexity and burstiness, striving to achieve high levels of both without sacrificing specificity or context. Use paragraphs that fully engage the reader, and write in a conversational style that is human-like. This means employing an informal tone, utilizing personal pronouns, keeping it simple, engaging the reader, utilizing the active voice, keeping it brief, asking rhetorical questions, and incorporating analogies and metaphors. Please end the article with a conclusion paragraph and 5 unique FAQs after the conclusion. Additionally, remember to bold the title and all headings of the article and use appropriate headings for H tags. Now, please write an article on the given topic: {qry}"
-    article = generate_content(prompt, model=model, max_tokens=max_tokens)
-    save_to_file(f"{qry}.txt", article)
-    with open("final_draft.txt") as file:
-        st.download_button(label=f"Download Final Draft ({qry})", data=file, key=qry)
-    return article'''
+#def quickArticleCreate(qry, model="gpt-3.5-turbo-16k", max_tokens=3000):
+    #prompt = f"Act as a skilled content writer who is proficient in SEO writing and has excellent English language skills. To get started, please create two tables. The first table should contain an outline of the article, and the second table should contain the article itself. Please use Markdown language to bold the heading of the second table. The article should be 4,000 words long, unique, SEO-optimized, and human-written in English. It should cover the given topic and include at least 15 headings and subheadings (including H1, H2, H3, and H4 headings). Please compose the article in your own words, avoiding copying and pasting from other sources. When producing content, each paragraph should be at least 250 words long, please consider complexity and burstiness, striving to achieve high levels of both without sacrificing specificity or context. Use paragraphs that fully engage the reader, and write in a conversational style that is human-like. This means employing an informal tone, utilizing personal pronouns, keeping it simple, engaging the reader, utilizing the active voice, keeping it brief, asking rhetorical questions, and incorporating analogies and metaphors. Please end the article with a conclusion paragraph and 5 unique FAQs after the conclusion. Additionally, remember to bold the title and all headings of the article and use appropriate headings for H tags. Now, please write an article on the given topic: {qry}"
+    #article = generate_content(prompt, model=model, max_tokens=max_tokens)
+    #save_to_file(f"{qry}.txt", article)
+    #with open("final_draft.txt") as file:
+        #st.download_button(label=f"Download Final Draft ({qry})", data=file, key=qry)
+    #return article
 
 def quickArticleCreate(qry, model, maxtokens):
     save_to_file(f"{qry.txt}", qry)
@@ -64,7 +64,7 @@ def main():
         for t in st.session_state.multiGenTopics:
             article = quickArticleCreate(t)
             fileList.append(article)
-            
+
         with zipfile.ZipFile("GeneratedArticles.zip", 'w') as myzip:
             for f in fileList:
                 myzip.write(f)
